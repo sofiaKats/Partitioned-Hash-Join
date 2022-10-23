10f57c11ab12f814a509df934022df6f3e073ded
sofiaKats <sophiakatsaouni@gmail.com>
2022-10-23 23:37:04 +0300

    corrected mock data, started hopscotch function

--- join-phase(hopscotch)/src/hopscotch.cpp
+++ join-phase(hopscotch)/src/hopscotch.cpp
@@ -4,12 +4,59 @@
 #include "hopscotch.h"
 using namespace std;
 
-Index::Index(const int value, const int H) : value(value)
+Index::Index() : has_value(false)
 {
     bitmap = new int[H];
+    // initializing bitmap with 0s
+    for (int i=0; i<H; i++) bitmap[i] = 0;
 }
 
 Index::~Index()
 {
     delete [] bitmap;
+}
+
+void Index::set_bitmap_index_to_1(const int index) {
+    bitmap[index] = 1;
+}
+
+void Index::set_bitmap_index_to_0(const int index) {
+    bitmap[index] = 0;
+}
+
+void Index::print_bitmap(void) {
+    for(int i=0; i<H; i++) cout << bitmap[i] ;
+    cout << endl;
+}
+
+//returns true if full, false if not full
+bool Index::is_bitmap_full(const int index) {
+    for(int i=0; i<H; i++)
+        if(bitmap[i] == 0) return false;
+    return true;
+}
+
+bool Index::get_has_value(void) { return has_value; }
+
+void Index::set_has_value(bool flag) { has_value = flag;}
+
+
+void hopscotch_hatching(Index** hashtable, int** mock_data, int table_size) {
+    // hashtable[0]->set_bitmap_index_to_1(0);
+    // hashtable[0]->print_bitmap();
+
+    // cout << endl;
+    // for(int i=0; i<8; i++) cout << "value: " << mock_data[i][0] << " hash: " << mock_data[i][1] << endl;
+
+    // for every number in the mock_data array
+    for (int counter=0; counter<8; counter++) {
+        int i = mock_data[counter][1]; //store pseudo hash value of number
+
+        // if(hashtable[i].is_bitmap_full) tote rehashing
+
+        for(int serial=i; serial<table_size; serial++)
+            if(hashtable[0]->get_has_value()) {
+                
+            }
+    }
 }
--- join-phase(hopscotch)/src/hopscotch.h
+++ join-phase(hopscotch)/src/hopscotch.h
@@ -2,13 +2,27 @@
 #include <cstring>
 using namespace std;
 
+#define H 3 
+
 // each index of the hopscoth table has a value and a corresponding bitmap
 class Index
 {
 private:
     int value;
+    bool has_value; //flag to check if position is empty
     int* bitmap;
 public:
-    Index(const int value, const int H);
+    Index();
     ~Index();
+    void set_bitmap_index_to_1(const int index); 
+    void set_bitmap_index_to_0(const int index); 
+    void print_bitmap(void);
+    bool get_has_value(void);
+    void set_has_value(bool flag);
+    bool is_bitmap_full(const int index); //returns true if full, false if not full
 };
+
+
+// given an empty hashtable and an array of data
+// perform hopscotch hashing
+void hopscotch_hatching(Index** hashtable, int** mock_data, int table_size);
